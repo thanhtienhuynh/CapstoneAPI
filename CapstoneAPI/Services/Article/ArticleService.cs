9e6cae812beed77a1669bfebf59de5e1923bc9fd
= <=>
2021-04-27 20:39:11 +0700

    Edit timezone in Article Service

--- CapstoneAPI/Services/Article/ArticleService.cs
+++ CapstoneAPI/Services/Article/ArticleService.cs
@@ -5,9 +5,12 @@ using CapstoneAPI.Helpers;
 using CapstoneAPI.Repositories;
 using CapstoneAPI.Services.UriService;
 using CapstoneAPI.Wrappers;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace CapstoneAPI.Services.Article
@@ -16,19 +19,24 @@ namespace CapstoneAPI.Services.Article
     {
         private IMapper _mapper;
         private readonly IUnitOfWork _uow;
-        //private readonly IUriService _uriService;
+        private readonly JObject configuration;
+
         public ArticleService(IUnitOfWork uow, IMapper mapper)
         {
             _uow = uow;
             _mapper = mapper;
-            //_uriService = uriService;
+            string path = Path.Combine(Path
+                .GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Configuration\TimeZoneConfiguration.json");
+            configuration = JObject.Parse(File.ReadAllText(path));
         }
         
         public async Task<PagedResponse<List<ArticleCollapseDataSet>>> GetListArticleForGuest(PaginationFilter validFilter, string route)
         {
             PagedResponse<List<ArticleCollapseDataSet>> result;
 
-            DateTime currentDate = DateTime.UtcNow.AddHours(7);
+            var currentTimeZone = configuration.SelectToken("CurrentTimeZone").ToString();
+
+            DateTime currentDate = DateTime.UtcNow.AddHours(int.Parse(currentTimeZone));
 
             IEnumerable<Models.Article> articles = await _uow.ArticleRepository
                 .Get(filter: a => a.Status == 1 && a.PublicFromDate != null && a.PublicToDate != null
@@ -36,11 +44,11 @@ namespace CapstoneAPI.Services.Article
                 orderBy: o => o.OrderByDescending(a => a.PostedDate),
                 first: validFilter.PageSize, offset: (validFilter.PageNumber - 1) * validFilter.PageSize);
 
-            var a = articles.Select(m => _mapper.Map<ArticleCollapseDataSet>(m)).ToList();
+            var articleCollapseDataSet = articles.Select(m => _mapper.Map<ArticleCollapseDataSet>(m)).ToList();
             var totalRecords = _uow.ArticleRepository
                 .Count(filter: a => a.Status == 1 && a.PublicFromDate != null && a.PublicToDate != null
                 && DateTime.Compare((DateTime)a.PublicToDate, currentDate) > 0);
-            result = PaginationHelper.CreatePagedReponse(a, validFilter, totalRecords, route);
+            result = PaginationHelper.CreatePagedReponse(articleCollapseDataSet, validFilter, totalRecords, route);
             return result;
         }
 
