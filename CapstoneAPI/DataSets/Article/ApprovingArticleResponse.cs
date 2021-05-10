ce227f07eb297463c3fe3c231d249cf090ad4abb
= <=>
2021-05-10 22:14:34 +0700

    Updae ApprovingArticle API

---
+++ CapstoneAPI/DataSets/Article/ApprovingArticleResponse.cs
@@ -0,0 +1,15 @@
+﻿using CapstoneAPI.DataSets.University;
+using System;
+using System.Collections.Generic;
+
+namespace CapstoneAPI.DataSets.Article
+{
+    public class ApprovingArticleResponse
+    {
+        public int Id { get; set; }
+        public DateTime? PublicFromDate { get; set; }
+        public DateTime? PublicToDate { get; set; }
+        public int? Status { get; set; }
+        public virtual ICollection<ApprovingArticleUniversityResponse> Universities { get; set; }
+    }
+}
