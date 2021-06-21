376d57728a1a81f22ddf575c0453bcb66441b134
= <=>
2021-06-21 16:40:13 +0700

    Add new CreateMajorSubjectGroup and CreateMajorSubjectWeight DataSet

---
+++ CapstoneAPI/DataSets/SubjectGroup/CreateMajorSubjectGroup.cs
@@ -0,0 +1,14 @@
+﻿using CapstoneAPI.DataSets.Subject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapstoneAPI.DataSets.SubjectGroup
+{
+    public class CreateMajorSubjectGroup
+    {
+        public int Id { get; set; }
+        public List<CreateMajorSubjectWeight> SubjectWeights { get; set; }
+    }
+}
