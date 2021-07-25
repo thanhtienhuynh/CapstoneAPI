01b1ffd980840941e75d635d85d9ab3b57e75bac
Tiến Huỳnh Thanh <tien01662892891@gmail.com>
2021-07-25 11:21:00 +0700

    remove unnecessary field in response get all seasons

--- CapstoneAPI/Features/Season/DataSet/AdminSeasonDataSet.cs
+++ CapstoneAPI/Features/Season/DataSet/AdminSeasonDataSet.cs
@@ -10,7 +10,5 @@ namespace CapstoneAPI.Features.Season.DataSet
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime FromDate { get; set; }
-        public DateTime? ToDate { get; set; }
-        public int Status { get; set; }
     }
 }
